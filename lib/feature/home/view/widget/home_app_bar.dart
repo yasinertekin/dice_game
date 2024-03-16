fd475cc1ccbcc12094802138cc419fd64b770e3d
Yasin Ertekin <yasinertekinn@outlook.com>
2024-03-16 20:45:59 +0300

    Add HomeAppBar and AppbarBottom widgets

---
+++ lib/feature/home/view/widget/home_app_bar.dart
@@ -0,0 +1,34 @@
+part of '../home_page.dart';
+
+final class _HomeAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const _HomeAppBar();
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: ProjectColor.silkyWhite.color,
+      title: const Text('Home Page'),
+      centerTitle: true,
+      bottom: const _AppbarBottom(),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
+
+final class _AppbarBottom extends StatelessWidget
+    implements PreferredSizeWidget {
+  const _AppbarBottom();
+
+  @override
+  Widget build(BuildContext context) {
+    return const PreferredSize(
+      preferredSize: Size.fromHeight(kToolbarHeight),
+      child: _CustomDivider(),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
