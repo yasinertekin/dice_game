c2e64224609a33e2c155cee1b1e9b76e1c1b6991
Yasin Ertekin <yasinertekinn@outlook.com>
2024-03-16 20:45:23 +0300

    Add main.dart file with initial app setup

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:dice_game/product/router/router_manager.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  runApp(const _DiceApp());
+}
+
+final class _DiceApp extends StatelessWidget {
+  const _DiceApp();
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      routerConfig: RouterManager.routes,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        bottomAppBarTheme: const BottomAppBarTheme(
+          color: Colors.transparent,
+          surfaceTintColor: Colors.transparent,
+          elevation: 0,
+        ),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+    );
+  }
+}
