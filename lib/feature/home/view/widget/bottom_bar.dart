eb21b92bc35160af6eb75e1b08fdfce920a361e6
Yasin Ertekin <yasinertekinn@outlook.com>
2024-03-16 20:45:44 +0300

    Add bottom bar widget to home page

---
+++ lib/feature/home/view/widget/bottom_bar.dart
@@ -0,0 +1,22 @@
+part of '../home_page.dart';
+
+final class _BottomBar extends StatelessWidget {
+  const _BottomBar();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        _CustomDivider(),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            _FavoriteIcon(),
+            _MenuIcon(),
+          ],
+        ),
+      ],
+    );
+  }
+}
