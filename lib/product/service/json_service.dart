ff037926e0694f73176b4427e2b325b137cb78b7
Yasin Ertekin <yasinertekinn@outlook.com>
2024-03-17 09:55:25 +0300

    Update DiceModel import and refactor getDestinations method

--- lib/product/service/json_service.dart
+++ lib/product/service/json_service.dart
@@ -1,30 +1,29 @@
 import 'dart:convert';
 
-import 'package:dice_game/product/model/dice_model.dart';
+import 'package:dice_game/product/model/dice_model/dice_model.dart';
 import 'package:flutter/services.dart';
 
 // ignore: one_member_abstracts
 /// IJsonService
-abstract class IJsonService {
+abstract interface class IJsonService {
   /// Get destinations
-  Future<List<DiceModel>> getDestinations();
+  Future<DiceModel> getDiceModel();
 }
 
 /// JsonService
 final class JsonService implements IJsonService {
   @override
-  Future<List<DiceModel>> getDestinations() async {
-    try {
-      final jsonString = await rootBundle.loadString('assets/dice.json');
-      final jsonList =
-          (json.decode(jsonString) as List).cast<Map<String, dynamic>>();
-      // JSON'daki her bir öğeyi Destination nesnesine dönüştür
-      final destinations = jsonList.map(DiceModel.fromJson).toList();
-
-      return destinations;
-    } catch (e) {
-      print('Error: $e');
-      return []; // Hata durumunda boş bir liste döndür
-    }
+  Future<DiceModel> getDiceModel() async {
+    final data = await rootBundle.loadString(_DicePath.dicePath.value);
+    final jsons = json.decode(data) as Map<String, dynamic>;
+    return DiceModel.fromJson(jsons);
   }
+}
+
+enum _DicePath {
+  dicePath('assets/data/data.json');
+
+  final String value;
+
+  const _DicePath(this.value);
 }
