ec02080f7555ff10e3bc5a50f90ce865bcb2b7d3
Yasin Ertekin <yasinertekinn@outlook.com>
2024-03-17 09:54:32 +0300

    Add HomeCubit to handle home state and fetch dice model

---
+++ lib/feature/home/cubit/home_cubit.dart
@@ -0,0 +1,21 @@
+import 'package:dice_game/feature/home/cubit/state/home_state.dart';
+import 'package:dice_game/product/service/json_service.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+final class HomeCubit extends Cubit<HomeState> {
+  HomeCubit(this._jsonService)
+      : super(const HomeState(status: HomeStatus.loading)) {
+    _getDiceModel();
+  }
+
+  final IJsonService _jsonService;
+
+  Future<void> _getDiceModel() async {
+    try {
+      final diceModel = await _jsonService.getDiceModel();
+      emit(HomeState(diceModel: diceModel, status: HomeStatus.loaded));
+    } catch (e) {
+      emit(const HomeState(status: HomeStatus.error));
+    }
+  }
+}
