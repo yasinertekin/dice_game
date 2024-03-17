b79a81295936901cc0003317fb2c520d04206a92
Yasin Ertekin <yasinertekin24@gmail.com>
2024-03-18 00:07:05 +0300

    Add CategoryDetailCard widget to category_detail_page

---
+++ lib/feature/category_detail/view/widget/category_detail_card.dart
@@ -0,0 +1,50 @@
+part of '../catergory_detail_page.dart';
+
+final class _CategoryDetailCard extends StatelessWidget {
+  const _CategoryDetailCard({
+    required this.diceList,
+    required this.index,
+  });
+
+  final List<DiceList>? diceList;
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: ProjectColor.red.toColor,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Spacer(
+            flex: 2,
+          ),
+          FittedBox(
+            child: SizedBox(
+              width: context.dynamicWidth(0.7),
+              child: Padding(
+                padding: context.paddingAllLow,
+                child: Text(
+                  diceList?[index].diceName ?? '',
+                  textAlign: TextAlign.center,
+                  style: context.textTheme.headlineLarge?.copyWith(
+                    color: ProjectColor.white.toColor,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          const Spacer(
+            flex: 5,
+          ),
+          CustomSvg(
+            assetPath: ProjectAssets.heard.toSvg,
+            height: context.dynamicHeight(0.1),
+          ),
+          const Spacer(),
+        ],
+      ),
+    );
+  }
+}
