b6903c58aac231d685c2eeb2e8e95208115ae757
Yasin Ertekin <yasinertekin24@gmail.com>
2024-03-18 14:37:12 +0300

    Add support for custom SVG assets

--- lib/product/widget/custom_svg.dart
+++ lib/product/widget/custom_svg.dart
@@ -1,3 +1,4 @@
+import 'package:dice_game/product/core/enum/project_assets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -12,7 +13,7 @@ final class CustomSvg extends StatelessWidget {
   });
 
   /// The path to the SVG file
-  final String assetPath;
+  final String? assetPath;
 
   /// The height of the SVG file
   final double height;
@@ -20,10 +21,50 @@ final class CustomSvg extends StatelessWidget {
   /// The fit of the SVG file
   final BoxFit fit;
 
+  String get _assetPath => assetPath?.isNotEmpty ?? false ? assetPath! : '';
+
+  @override
+  Widget build(BuildContext context) {
+    return _assetPath.isEmpty
+        ? _DefaultIcon(height: height, fit: fit)
+        : _CustomAssets(assetPath: _assetPath, height: height, fit: fit);
+  }
+}
+
+final class _CustomAssets extends StatelessWidget {
+  const _CustomAssets({
+    required String assetPath,
+    required this.height,
+    required this.fit,
+  }) : _assetPath = assetPath;
+
+  final String _assetPath;
+  final double height;
+  final BoxFit fit;
+
   @override
   Widget build(BuildContext context) {
     return SvgPicture.asset(
-      assetPath,
+      _assetPath,
+      height: height,
+      fit: fit,
+    );
+  }
+}
+
+final class _DefaultIcon extends StatelessWidget {
+  const _DefaultIcon({
+    required this.height,
+    required this.fit,
+  });
+
+  final double height;
+  final BoxFit fit;
+
+  @override
+  Widget build(BuildContext context) {
+    return SvgPicture.asset(
+      ProjectAssets.heard.toSvg,
       height: height,
       fit: fit,
     );
