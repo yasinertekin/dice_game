755ee2f53114844904928404f65961ae20f9a378
Yasin Ertekin <yasinertekin24@gmail.com>
2024-03-18 14:34:46 +0300

    Add Hive initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:dice_game/product/initialize/theme/custom_theme.dart';
 import 'package:dice_game/product/router/app_router.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
   runApp(
     _DiceApp(),
   );
