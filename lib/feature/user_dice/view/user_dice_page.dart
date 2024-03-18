df6af9209998aab997bbda627df42c921911bed0
Yasin Ertekin <yasinertekin24@gmail.com>
2024-03-18 14:35:58 +0300

    Add user dice functionality to UserDicePage

--- lib/feature/user_dice/view/user_dice_page.dart
+++ lib/feature/user_dice/view/user_dice_page.dart
@@ -1,41 +1,72 @@
 import 'package:auto_route/annotations.dart';
+import 'package:dice_game/feature/user_dice/cubit/state/user_dice_state.dart';
+import 'package:dice_game/feature/user_dice/cubit/user_dice_cubit.dart';
+import 'package:dice_game/product/core/cache/cache_manager.dart';
+import 'package:dice_game/product/core/enum/hive_key.dart';
+import 'package:dice_game/product/model/category_dices/category_dices.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 @RoutePage()
-final class UserDicePage extends StatelessWidget {
-  const UserDicePage({required this.myParameter, super.key});
 
-  final String myParameter;
+/// A page to display the user dice
+final class UserDicePage extends StatelessWidget {
+  /// Creates a new instance of the [UserDicePage] class.
+  const UserDicePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return _UserDiceView(
-      myParameter,
+    return BlocProvider<UserDiceCubit>(
+      create: (context) => UserDiceCubit(
+        CategoryDicesCacheManager(HiveKey.categoryDicesManagerKey.value),
+      )..fetchUserDice(),
+      child: const _UserDiceView(),
     );
   }
 }
 
 final class _UserDiceView extends StatelessWidget {
-  const _UserDiceView(
-    this.myParameter,
-  );
-
-  final dynamic myParameter;
+  const _UserDiceView();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          context.read<UserDiceCubit>().addUserDice(
+                CategoryDices(
+                  diceName: 'Dice 1',
+                ),
+              );
+        },
+        child: const Icon(Icons.add),
+      ),
       appBar: AppBar(
         title: const Text('User Dice'),
         actions: const [],
       ),
-      body: Column(
-        children: [
-          Text('My Parameter: $myParameter'),
-          const Center(
-            child: Text('User Dice Page'),
-          ),
-        ],
+      body: BlocBuilder<UserDiceCubit, UserDiceState>(
+        builder: (context, state) {
+          if (state.isLoading ?? false) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (state.error?.isNotEmpty ?? false) {
+            return Center(
+              child: Text(state.error!),
+            );
+          } else {
+            return ListView.builder(
+              itemCount: state.categoryDices.length,
+              itemBuilder: (context, index) {
+                final categoryDice = state.categoryDices[index];
+                return ListTile(
+                  title: Text(categoryDice.diceName ?? ''),
+                );
+              },
+            );
+          }
+        },
       ),
     );
   }
