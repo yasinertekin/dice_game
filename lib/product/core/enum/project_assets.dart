5283c80dbb66d4dd2e531b7dde3e7bc99628ac90
Yasin Ertekin <yasinertekin24@gmail.com>
2024-03-18 16:24:21 +0300

    Update project assets enum

--- lib/product/core/enum/project_assets.dart
+++ lib/product/core/enum/project_assets.dart
@@ -1,10 +1,19 @@
 /// ProjectAssets is a enum class that contains the path of the assets
 enum ProjectAssets {
   /// The path to the favorite icon (SvG file)
-  heard('favorite'),
+  icHeard('favorite'),
 
   /// The path to the menu icon (SvG file)
-  menu('menu');
+  icMenu('menu'),
+
+  /// The path to the app logo (Png file)
+  imgAppLogo('app_logo'),
+
+  /// The path to the splash image (Png file)
+  imgSplash('splash'),
+
+  /// The path to the splash subtitle (Png file)
+  imgSplashSubtitle('splash_subtitle');
 
   /// The value of the enum
   final String value;
@@ -13,7 +22,7 @@ enum ProjectAssets {
   const ProjectAssets(this.value);
 
   /// The path to the PNG file
-  String get toPng => 'assets/image/img_$value.png';
+  String get toPng => 'assets/image/project_image/img_$value.png';
 
   /// The path to the SVG file
   String get toSvg => 'assets/icon/project_icon/ic_$value.svg';
