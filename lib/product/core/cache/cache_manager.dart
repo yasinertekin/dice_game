3bcd88b3df4b3eb47d0da1fb6ac392ab5865e797
Yasin Ertekin <yasinertekin24@gmail.com>
2024-03-18 14:36:11 +0300

    Add CategoryDicesCacheManager class to cache manager and remove UserCacheManager class

--- lib/product/core/cache/cache_manager.dart
+++ lib/product/core/cache/cache_manager.dart
@@ -1,3 +1,5 @@
+import 'package:dice_game/product/core/constants/hive_type_id_constants.dart';
+import 'package:dice_game/product/model/category_dices/category_dices.dart';
 import 'package:hive/hive.dart';
 
 /// Represents a cache manager.
@@ -49,4 +51,61 @@ abstract final class ICacheManager<T> {
 
   /// Updates the item.
   Future<void> updateItem(T item);
+}
+
+/// Represents a cache manager for the [CategoryDices] model.
+final class CategoryDicesCacheManager extends ICacheManager<CategoryDices> {
+  /// Creates a new instance of the [CategoryDicesCacheManager] class.
+  CategoryDicesCacheManager(super.key);
+
+  @override
+  Future<void> addItems(List<CategoryDices> items) async {
+    await _boxBooks?.addAll(items);
+  }
+
+  @override
+  CategoryDices? getItem(String key) {
+    return _boxBooks?.get(key);
+  }
+
+  @override
+  List<CategoryDices> getValues() {
+    return _boxBooks?.values.toList() ?? [];
+  }
+
+  @override
+  Future<void> putItem(String key, CategoryDices item) async {
+    return _boxBooks?.put(key, item);
+  }
+
+  @override
+  Future<void> putItems(List<CategoryDices> items) async {
+    await _boxBooks?.putAll(
+      {for (final item in items) item.id: item},
+    );
+  }
+
+  @override
+  Future<void> removeItem(String key) async {
+    await _boxBooks?.delete(key);
+  }
+
+  @override
+  Future<void> removeModel(CategoryDices item) {
+    return removeItem(item.id ?? '');
+  }
+
+  @override
+  Future<void> updateItem(CategoryDices item) {
+    return putItem(item.id ?? '', item);
+  }
+
+  @override
+  void saveDataAdapter() {
+    if (!Hive.isAdapterRegistered(HiveTypeIdConstants.categoryDicesId)) {
+      Hive.registerAdapter(
+        CategoryDicesAdapter(),
+      );
+    }
+  }
 }
