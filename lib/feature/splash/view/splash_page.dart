8cf5d1b2712c3adafc919ab94d7c2f090de37604
Yasin Ertekin <yasinertekin24@gmail.com>
2024-03-18 16:24:03 +0300

    Add imports and update UI in splash_page.dart

--- lib/feature/splash/view/splash_page.dart
+++ lib/feature/splash/view/splash_page.dart
@@ -1,6 +1,11 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:dice_game/product/core/enum/project_assets.dart';
+import 'package:dice_game/product/core/enum/project_color.dart';
 import 'package:dice_game/product/router/app_router.gr.dart';
 import 'package:flutter/material.dart';
+import 'package:kartal/kartal.dart';
+
+part 'mixin/splash_view_mixin.dart';
 
 /// Splash Page
 @RoutePage()
@@ -14,28 +19,33 @@ final class SplashPage extends StatelessWidget {
   }
 }
 
-final class _SplashView extends StatelessWidget {
+final class _SplashView extends StatefulWidget {
   const _SplashView();
 
   @override
+  State<_SplashView> createState() => _SplashViewState();
+}
+
+final class _SplashViewState extends State<_SplashView> with SplashViewMixin {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          const Expanded(
-            child: Center(
-              child: Text('Splash Page'),
+      backgroundColor: ProjectColor.buzzIn.toColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              ProjectAssets.imgAppLogo.toPng,
+              height: context.sized.dynamicHeight(0.1),
             ),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              context.router.push(
-                const HomeRoute(),
-              );
-            },
-            child: const Text('Next Page'),
-          ),
-        ],
+            Image.asset(
+              ProjectAssets.imgSplashSubtitle.toPng,
+              height: context.sized.dynamicHeight(0.1),
+            ),
+            const Text('İşini Şansa Bırak'),
+          ],
+        ),
       ),
     );
   }
