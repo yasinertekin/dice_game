29288a723a5301412d27e345e6800bf977fb2421
Yasin Ertekin <yasinertekin24@gmail.com>
2024-03-18 14:37:07 +0300

    Update DiceModel to Category in JsonService

--- lib/product/service/json_service.dart
+++ lib/product/service/json_service.dart
@@ -1,22 +1,22 @@
 import 'dart:convert';
 
-import 'package:dice_game/product/model/dice_model/dice_model.dart';
+import 'package:dice_game/product/model/category/category.dart';
 import 'package:flutter/services.dart';
 
 // ignore: one_member_abstracts
 /// IJsonService
 abstract interface class IJsonService {
   /// Get destinations
-  Future<DiceModel> getDiceModel();
+  Future<Category> getDiceModel();
 }
 
 /// JsonService
 final class JsonService implements IJsonService {
   @override
-  Future<DiceModel> getDiceModel() async {
+  Future<Category> getDiceModel() async {
     final data = await rootBundle.loadString(_DicePath.dicePath.value);
     final jsons = json.decode(data) as Map<String, dynamic>;
-    return DiceModel.fromJson(jsons);
+    return Category.fromJson(jsons);
   }
 }
 
