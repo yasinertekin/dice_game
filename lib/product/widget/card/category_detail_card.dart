cc74a544074e6e824bbc0941f8e305016634b248
Yasin Ertekin <yasinertekin24@gmail.com>
2024-03-20 14:59:23 +0300

    Add CategoryDetailCard widget

---
+++ lib/product/widget/card/category_detail_card.dart
@@ -0,0 +1,67 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:dice_game/product/core/enum/project_assets.dart';
+import 'package:dice_game/product/core/enum/project_color.dart';
+import 'package:dice_game/product/core/extension/context_extension.dart';
+import 'package:dice_game/product/core/model/category_dices/category_dices.dart';
+import 'package:dice_game/product/utils/router/app_router.gr.dart';
+import 'package:dice_game/product/widget/image/custom_svg.dart';
+import 'package:flutter/material.dart';
+
+final class CategoryDetailCard extends StatelessWidget {
+  const CategoryDetailCard({
+    required this.diceList,
+    required this.index,
+    super.key,
+  });
+
+  final List<CategoryDices>? diceList;
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        context.router.push(
+          DiceDescriptionRoute(
+            categoryDices: diceList![index],
+          ),
+        );
+      },
+      child: Card(
+        color: ProjectColor.red.toColor,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Spacer(
+              flex: 2,
+            ),
+            FittedBox(
+              child: SizedBox(
+                width: context.dynamicWidth(0.7),
+                child: Padding(
+                  padding: context.paddingAllLow,
+                  child: Text(
+                    diceList?[index].diceName ?? '',
+                    textAlign: TextAlign.center,
+                    style: context.textTheme.headlineLarge?.copyWith(
+                      color: ProjectColor.white.toColor,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const Spacer(
+              flex: 5,
+            ),
+            CustomSvg(
+              assetPath: ProjectAssets.icHeard.toSvg,
+              height: context.dynamicHeight(0.1),
+            ),
+            const Spacer(),
+          ],
+        ),
+      ),
+    );
+  }
+}
