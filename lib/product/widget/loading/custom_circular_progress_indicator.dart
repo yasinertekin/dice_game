dcf75bce51a4c8be5d774f0164be24b90ee5505b
Yasin Ertekin <yasinertekin24@gmail.com>
2024-03-20 14:59:40 +0300

    Add CustomCircularProgressIndicator widget

---
+++ lib/product/widget/loading/custom_circular_progress_indicator.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+/// CustomCircularProgressIndicator
+final class CustomCircularProgressIndicator extends StatelessWidget {
+  /// CustomCircularProgressIndicator
+  const CustomCircularProgressIndicator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+}
