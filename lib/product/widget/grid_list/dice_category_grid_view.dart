7e18e919f333019aaa9280ceba38cf19f637bddd
Yasin Ertekin <yasinertekin24@gmail.com>
2024-03-20 14:59:33 +0300

    Add DiceCategoryGridView widget to display a grid of category dices

---
+++ lib/product/widget/grid_list/dice_category_grid_view.dart
@@ -0,0 +1,38 @@
+import 'package:dice_game/product/core/model/category_dices/category_dices.dart';
+import 'package:dice_game/product/widget/card/category_detail_card.dart';
+import 'package:flutter/material.dart';
+
+/// CustomCategoryBuilder
+final class DiceCategoryGridView extends StatelessWidget {
+  /// CustomCategoryBuilder
+  const DiceCategoryGridView({
+    required this.diceList,
+    super.key,
+  });
+
+  /// DiceList
+  final List<CategoryDices>? diceList;
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      gridDelegate: const _SliverGridDelegate(),
+      itemCount: diceList?.length ?? 0,
+      itemBuilder: (context, index) {
+        return CategoryDetailCard(
+          diceList: diceList,
+          index: index,
+        );
+      },
+    );
+  }
+}
+
+final class _SliverGridDelegate
+    extends SliverGridDelegateWithFixedCrossAxisCount {
+  const _SliverGridDelegate()
+      : super(
+          crossAxisCount: 2,
+          crossAxisSpacing: 15,
+        );
+}
