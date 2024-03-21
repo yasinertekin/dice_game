1a86abe11a962efef36cac283ae34254c0d9657b
Yasin Ertekin <yasinertekin24@gmail.com>
2024-03-21 16:41:32 +0300

    Refactor main.dart to use dependency injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,23 @@
+import 'package:dice_game/locator.dart';
 import 'package:dice_game/product/core/initialize/app_initialize.dart';
 import 'package:dice_game/product/core/initialize/state_initialize.dart';
-import 'package:dice_game/product/utils/router/app_router.dart';
 import 'package:dice_game/product/utils/theme/custom_theme.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   await AppInitialize.initialize();
   runApp(
-    StateInitialize(child: _DiceApp()),
+    const StateInitialize(child: _DiceApp()),
   );
 }
 
 final class _DiceApp extends StatelessWidget {
-  _DiceApp();
-
-  final _appRouter = AppRouter();
+  const _DiceApp();
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final appRouter = Locator.appRouter;
     return MaterialApp.router(
       title: 'Flutter Demo',
 
@@ -26,8 +25,8 @@ final class _DiceApp extends StatelessWidget {
       theme: CustomTheme().themeData,
 
       /// This is the router of the app
-      routerDelegate: _appRouter.delegate(),
-      routeInformationParser: _appRouter.defaultRouteParser(),
+      routerDelegate: appRouter.delegate(),
+      routeInformationParser: appRouter.defaultRouteParser(),
 
       debugShowCheckedModeBanner: false,
     );
