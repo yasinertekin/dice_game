88788a8892949f0343fb7c16a8f51c4d1aac8a06
Yasin Ertekin <yasinertekin24@gmail.com>
2024-03-21 16:43:05 +0300

    Add NavigationManager mixin for page navigation

---
+++ lib/product/core/mixin/navigation_manager.dart
@@ -0,0 +1,17 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:dice_game/locator.dart';
+
+/// A mixin that is used to navigate between pages
+base mixin NavigationManager {
+  /// This method is used to navigate to the page
+  void navigatePush(PageRouteInfo pageRouteInfo) {
+    Locator.appRouter.push<void>(
+      pageRouteInfo,
+    );
+  }
+
+  /// This method is used to navigate to the previous page
+  void navigatePop() {
+    Locator.appRouter.maybePop<void>();
+  }
+}
