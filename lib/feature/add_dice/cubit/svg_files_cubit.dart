4519b0b593e45cbcdc815674baf42b6240d6d181
Yasin Ertekin <yasinertekinn@outlook.com>
2024-03-23 00:58:25 +0300

    Add selectedIconIndex and selectIcon method to SVGFilesCubit

--- lib/feature/add_dice/cubit/svg_files_cubit.dart
+++ lib/feature/add_dice/cubit/svg_files_cubit.dart
@@ -6,6 +6,17 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 final class SVGFilesCubit extends Cubit<List<String>> {
   SVGFilesCubit() : super([]);
 
+  int selectedIconIndex = -1;
+
+  /// Seçili indexsin colorunu değiştirir
+  void selectIcon(int index) {
+    selectedIconIndex = index;
+    emit([...state]);
+  }
+
+  /// Selected icon
+  String get selectedIcon => state[selectedIconIndex];
+
   Future<void> listSVGFiles(String directory) async {
     final svgFiles = <String>[];
 
