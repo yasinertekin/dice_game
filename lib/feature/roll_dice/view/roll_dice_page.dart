d5f1446531ea85d95a352d38287138adbba05f56
Yasin Ertekin <yasinertekinn@outlook.com>
2024-03-23 11:48:54 +0300

    Add RollDiceCubit and related widgets

--- lib/feature/roll_dice/view/roll_dice_page.dart
+++ lib/feature/roll_dice/view/roll_dice_page.dart
@@ -1,27 +1,46 @@
-import 'dart:math';
-
 import 'package:auto_route/auto_route.dart';
+import 'package:dice_game/feature/roll_dice/cubit/roll_dice_cubit.dart';
+import 'package:dice_game/product/core/enum/project_assets.dart';
 import 'package:dice_game/product/core/enum/project_color.dart';
+import 'package:dice_game/product/core/extension/context_extension.dart';
 import 'package:dice_game/product/core/model/sub_dices/sub_dices.dart';
+import 'package:dice_game/product/widget/button/custom_back_button.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/src/scheduler/ticker.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lottie/lottie.dart';
+
+part 'mixin/roll_dice_view_mixin.dart';
+part 'widget/dice_lottie.dart';
+part 'widget/random_options_title.dart';
+part 'widget/reset_button.dart';
+part 'widget/roll_dice_app_bar.dart';
 
 @RoutePage()
+
+/// RollDicePage is a page that shows a dice animation and random options.
 final class RollDicePage extends StatelessWidget {
+  /// RollDicePage is a page that shows a dice animation and random options.
   const RollDicePage({
     required this.options,
     super.key,
   });
+
+  /// Options for the dice.
   final SubDices options;
 
   @override
   Widget build(BuildContext context) {
-    return _RollDiceView(
-      options: options,
+    return BlocProvider(
+      create: (context) => RollDiceCubit(),
+      child: _RollDiceView(
+        options: options,
+      ),
     );
   }
 }
 
-final class _RollDiceView extends StatelessWidget {
+final class _RollDiceView extends StatefulWidget {
   const _RollDiceView({
     required this.options,
   });
@@ -29,39 +48,77 @@ final class _RollDiceView extends StatelessWidget {
   final SubDices? options;
 
   @override
+  State<_RollDiceView> createState() => _RollDiceViewState();
+}
+
+final class _RollDiceViewState extends State<_RollDiceView>
+    with _RollDiceViewMixin {
+  @override
   Widget build(BuildContext context) {
-    // Select a random option from the list
-    final random = Random();
-    final randomIndex = random.nextInt(options!.options!.length);
     return Scaffold(
       backgroundColor: ProjectColor.silkyWhite.toColor,
-      appBar: AppBar(
-        backgroundColor: ProjectColor.concreteSideWalk.toColor,
-        title: const Text('Roll Dice'),
-      ),
+      appBar: const _RollDiceAppBar(),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              options!.options![randomIndex].name.toString(),
-              style: const TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                print(options?.options?.length);
-              },
-              child: const Text('Roll the dice'),
-            ),
-            const SizedBox(height: 16),
-            const Text('Roll the dice'),
-            const SizedBox(height: 16),
+            _DiceLottie(controller: _controller),
+            _RandomOptionsBuilder(widget: widget, controller: _controller),
           ],
         ),
       ),
     );
+  }
+
+  @override
+  Ticker createTicker(TickerCallback onTick) {
+    return Ticker(onTick);
+  }
+}
+
+final class _RandomOptionsBuilder extends StatelessWidget {
+  const _RandomOptionsBuilder({
+    required this.widget,
+    required AnimationController controller,
+  }) : _controller = controller;
+
+  final _RollDiceView widget;
+  final AnimationController _controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<RollDiceCubit, RollDiceState>(
+      builder: (context, state) {
+        final cubit = context.read<RollDiceCubit>();
+        if (state == RollDiceState.completed) {
+          final randomIndex = context
+              .read<RollDiceCubit>()
+              .getRandomIndex(widget.options?.options);
+          return Column(
+            children: [
+              _RandomOptionsTitle(
+                widget: widget,
+                randomIndex: randomIndex,
+              ),
+              SizedBox(
+                height: context.dynamicHeight(0.1),
+              ),
+              _ResetButton(cubit: cubit, controller: _controller),
+            ],
+          );
+        } else {
+          return const _EmptyWidget();
+        }
+      },
+    );
+  }
+}
+
+final class _EmptyWidget extends StatelessWidget {
+  const _EmptyWidget();
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox.shrink();
   }
 }
