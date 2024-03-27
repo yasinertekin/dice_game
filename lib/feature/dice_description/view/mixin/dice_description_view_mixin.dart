24ed29db18a1d87d5a97b5b18481456d11a46c91
Yasin Ertekin <yasinertekin24@gmail.com>
2024-03-27 14:03:44 +0300

    Refactor _showDatePicker method to handle adult content

--- lib/feature/dice_description/view/mixin/dice_description_view_mixin.dart
+++ lib/feature/dice_description/view/mixin/dice_description_view_mixin.dart
@@ -9,7 +9,39 @@ base mixin _DiceDescriptionViewMixin on State<_DiceDescriptionView> {
     );
   }
 
-  void _showDatePicker() {}
+  Future<void> _showDatePicker() async {
+    if (widget.categoryDices.isAdultContent ?? false) {
+      final pickedDate = await showDatePicker(
+        useRootNavigator: false,
+        barrierDismissible: false,
+        context: context,
+        initialDate: DateTime.now(),
+        confirmText: 'Seç',
+        cancelText: 'İptal',
+        barrierColor: Colors.black.withOpacity(0.5),
+        barrierLabel: 'Tarih Seç',
+        helpText: 'Doğum Tarihinizi Seçin',
+        errorFormatText: 'Geçerli bir tarih seçin',
+        errorInvalidText: 'Geçerli bir tarih seçin',
+        fieldLabelText: 'Doğum Tarihi',
+        firstDate: DateTime(1900),
+        lastDate: DateTime.now(),
+        keyboardType: TextInputType.datetime,
+      );
+      if (pickedDate == null) {
+        await Locator.appRouter.navigate(const HomeRoute());
+        return;
+      }
+
+      final today = DateTime.now();
+      final minimumDate =
+          today.subtract(const Duration(days: 18 * 365)); // 18 years ago
+      if (pickedDate.isAfter(minimumDate)) {
+        _showAlerDialog();
+        return;
+      }
+    }
+  }
 
   void _showAlerDialog() {}
 }
