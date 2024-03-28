8b1bd43fe0e265ea0c3c3f620605f5d31e4bfb10
Yasin Ertekin <yasinertekin24@gmail.com>
2024-03-28 10:36:01 +0300

    Add Google Mobile Ads initialization

--- lib/product/core/initialize/app_initialize.dart
+++ lib/product/core/initialize/app_initialize.dart
@@ -1,5 +1,6 @@
 import 'package:dice_game/locator.dart';
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 @immutable
@@ -11,6 +12,7 @@ final class AppInitialize {
   /// This method is used to initialize the app
   static Future<void> initialize() async {
     WidgetsFlutterBinding.ensureInitialized();
+    await MobileAds.instance.initialize();
     await Locator.setup();
     await Hive.initFlutter();
   }
