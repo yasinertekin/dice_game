c890f7f0b1b5ee875898477b107a87109e19d3d2
Yasin Ertekin <yasinertekin24@gmail.com>
2024-04-01 10:49:01 +0300

    Refactor HomeCubit class and update getDiceModel method

--- lib/feature/home/cubit/home_cubit.dart
+++ lib/feature/home/cubit/home_cubit.dart
@@ -1,18 +1,17 @@
 import 'package:dice_game/feature/home/cubit/state/home_state.dart';
 import 'package:dice_game/product/core/service/json_service.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 final class HomeCubit extends Cubit<HomeState> {
-  HomeCubit(this._jsonService)
-      : super(const HomeState(status: HomeStatus.loading)) {
-    _getDiceModel();
-  }
+  HomeCubit(this._jsonService, BuildContext context)
+      : super(const HomeState(status: HomeStatus.loading));
 
   final IJsonService _jsonService;
 
-  Future<void> _getDiceModel() async {
+  Future<void> getDiceModel(BuildContext context) async {
     try {
-      final diceModel = await _jsonService.getDiceModel();
+      final diceModel = await _jsonService.getDiceModel(context);
       emit(HomeState(diceModel: diceModel, status: HomeStatus.loaded));
     } catch (e) {
       emit(const HomeState(status: HomeStatus.error));
