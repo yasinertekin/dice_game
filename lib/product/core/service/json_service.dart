6a2263c3b4047eb22230a1dade5a115caee778e2
Yasin Ertekin <yasinertekin24@gmail.com>
2024-04-01 10:50:28 +0300

    Add localization support and update JSON service

--- lib/product/core/service/json_service.dart
+++ lib/product/core/service/json_service.dart
@@ -1,27 +1,34 @@
 import 'dart:convert';
 
 import 'package:dice_game/product/core/model/category/category.dart';
+import 'package:dice_game/product/utils/localization/localization_manager.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 // ignore: one_member_abstracts
 /// IJsonService
 abstract interface class IJsonService {
   /// Get destinations
-  Future<Category> getDiceModel();
+  Future<Category> getDiceModel(BuildContext context);
 }
 
 /// JsonService
 final class JsonService implements IJsonService {
   @override
-  Future<Category> getDiceModel() async {
-    final data = await rootBundle.loadString(_DicePath.dicePath.value);
+  Future<Category> getDiceModel(BuildContext context) async {
+    final data = context.locale == Locales.en.locale
+        ? await rootBundle.loadString(_DicePath.dicePathEn.value)
+        : await rootBundle.loadString(_DicePath.dicePath.value);
+
     final jsons = json.decode(data) as Map<String, dynamic>;
     return Category.fromJson(jsons);
   }
 }
 
 enum _DicePath {
-  dicePath('assets/data/data.json');
+  dicePathEn('assets/data/en.json'),
+  dicePath('assets/data/tr.json');
 
   final String value;
 
---
+++ lib/product/utils/localization/locale_keys.g.dart
@@ -0,0 +1,13 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+abstract class  LocaleKeys {
+  static const home_categories = 'home.categories';
+  static const home_usersDice = 'home.usersDice';
+  static const home = 'home';
+  static const favorites_favorites_dice = 'favorites.favorites_dice';
+  static const favorites = 'favorites';
+  static const settings_settings_title = 'settings.settings_title';
+  static const settings_select_dice = 'settings.select_dice';
+  static const settings = 'settings';
+
+}
