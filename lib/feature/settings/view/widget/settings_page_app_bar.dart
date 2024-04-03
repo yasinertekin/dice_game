08d1632c4ca55d8a55bb1764b8d9c7f6dcef6bbf
Yasin Ertekin <yasinertekin24@gmail.com>
2024-04-03 08:56:08 +0300

    Refactor settings page app bar

--- lib/feature/settings/view/widget/settings_page_app_bar.dart
+++ lib/feature/settings/view/widget/settings_page_app_bar.dart
@@ -12,12 +12,7 @@ final class _SettingsPageAppBar extends StatelessWidget
       ],
       leading: const CustomBackButton(),
       backgroundColor: ProjectColor.concreteSideWalk.toColor,
-      title: CustomText(
-        text: LocaleKeys.settings_settings_title,
-        style: context.textTheme.titleLarge?.copyWith(
-          color: ProjectColor.white.toColor,
-        ),
-      ),
+      title: const _AppBarTitle(),
     );
   }
 
@@ -25,36 +20,69 @@ final class _SettingsPageAppBar extends StatelessWidget
   Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 }
 
-final class _ChangeLanguageButton extends StatelessWidget {
+final class _AppBarTitle extends StatelessWidget {
+  const _AppBarTitle();
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomText(
+      text: LocaleKeys.settings_settings_title,
+      style: context.textTheme.titleLarge?.copyWith(
+        color: ProjectColor.white.toColor,
+      ),
+    );
+  }
+}
+
+final class _ChangeLanguageButton extends StatelessWidget
+    with _ChangeLanguageButtonMixin {
   const _ChangeLanguageButton();
+
+  @override
+  void customShowCupertinoModalPopup(BuildContext context) {
+    final cubit = context.read<HomeCubit>();
+    showCupertinoModalPopup<void>(
+      context: context,
+      builder: (context) {
+        return _CustomCupertinoActionSheet(cubit: cubit);
+      },
+    );
+  }
+
+  @override
+  void customShowMaterialModalBottomSheet(BuildContext context) {
+    super.customShowMaterialModalBottomSheet(context);
+    final cubit = context.read<HomeCubit>();
+    showModalBottomSheet<void>(
+      context: context,
+      builder: (context) {
+        return _CustomMaterialActionSheet(cubit: cubit);
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return IconButton(
-      onPressed: () => _changeLanguage(context),
-      icon: const Icon(Icons.language),
+      onPressed: () {
+        /// CupertinoContextMenu is used for iOS devices.
+        if (Platform.isIOS) {
+          customShowCupertinoModalPopup(context);
+        } else {
+          /// PopupMenuButton is used for Android devices.
+          customShowMaterialModalBottomSheet(context);
+        }
+      },
+      icon: const _LanguageIcon(),
     );
   }
+}
 
-  Future<void> _changeLanguage(BuildContext context) async {
-    if (context.locale == Locales.tr.locale) {
-      await ProductLocalization.updateLanguage(
-        context: context,
-        value: Locales.en,
-      );
-      await context.read<HomeCubit>().getDiceModel(context);
-    } else if (context.locale == Locales.en.locale) {
-      await ProductLocalization.updateLanguage(
-        context: context,
-        value: Locales.tr,
-      );
-      await context.read<HomeCubit>().getDiceModel(context);
-    } else {
-      await ProductLocalization.updateLanguage(
-        context: context,
-        value: Locales.en,
-      );
-      await context.read<HomeCubit>().getDiceModel(context);
-    }
+final class _LanguageIcon extends StatelessWidget {
+  const _LanguageIcon();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Icon(Icons.language);
   }
 }
